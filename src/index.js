a8865300cc8e8697f7aca8f943b76c3c68d5628e
JDMira98 <juandiego.miramoreno@gmail.com>
2024-09-08 00:48:55 -0500

    first commit 1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { TodoApp } from './components/08-useReducer/TodoApp';
+
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
+
